5c51d7f929fdf2a78822a1ba185910595d0be170
amir <amirh4liyan@gmail.com>
2022-01-07 15:08:17 +0330

    now app can parser some value and login to account, also it can read username from files

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
 #!/usr/bin/python3
 
-print("Hello World!")
+import argparse
+from instagrapi import Client
+
+# Arguments
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+parser.add_argument("-d", "--dontlogin", action="store_true",
+                    help="prevent from login to account")
+group.add_argument("-f", "--follow", help="")
+group.add_argument("-u", "--unfollow", help="")
+args = parser.parse_args()
+
+# Login
+cl = Client()
+if not args.dontlogin:
+    User, Pass = "aryaramnes20", "@mir8157"
+    cl.login(User, Pass)
+
+if args.follow:
+    path = args.follow
+elif args.unfollow:
+    path = args.unfollow
+
+with open(path) as f:
+    content = f.readlines()
+
+guys = set()
+for item in content:
+    if "usr" in item:
+        item = item.strip()
+        guys.add(item.split()[1])
+
+print(guys)
